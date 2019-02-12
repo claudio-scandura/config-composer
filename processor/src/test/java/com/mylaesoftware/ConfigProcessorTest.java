619da12d5ece6d5af1aa560ffb7acd407c7ba417
Claudio Scandura <cl.scandura@gmail.com>
2019-02-12 10:21:46 +0000

    Add tests for happy path.

--- processor/src/test/java/com/mylaesoftware/ConfigProcessorTest.java
+++ processor/src/test/java/com/mylaesoftware/ConfigProcessorTest.java
@@ -6,47 +6,102 @@ import com.google.testing.compile.Compiler;
 import com.google.testing.compile.JavaFileObjects;
 import com.mylaesoftware.specs.ConfigSpec;
 import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.function.Consumer;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.lang3.StringUtils.capitalize;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConfigProcessorTest {
 
+  private final static String ANY_NAME = "[a-zA-Z0-9_]+";
+
   private final static String INPUT_SOURCE_NAME = "TestInterface";
 
-  private static String DEFAULT_INPUT = "import com.mylaesoftware.Config;\n" +
+  private final static String CONFIG_FIELD_KEY = "path.to.property";
+  private final static String CONFIG_FIELD_TYPE = "String";
+  private final static String CONFIG_FIELD_NAME = "property";
+  private final static String NON_CONFIG_FIELD_NAME = "other";
+
+  private static String DEFAULT_INPUT = String.format("import com.mylaesoftware.Config;\n" +
       "import com.mylaesoftware.ConfigValue;\n" +
       "\n" +
       "@Config\n" +
-      "interface " + INPUT_SOURCE_NAME + " {\n" +
+      "interface %s {\n" +
       "\n" +
-      "  @ConfigValue(atKey = \"someKey\")\n" +
-      "  String getWhatever();\n" +
+      "  @ConfigValue(atKey = \"%s\")\n" +
+      "  %s %s();\n" +
+      "  default String %s() {return \"\";}\n" +
       "\n" +
-      "}";
+      "}", INPUT_SOURCE_NAME, CONFIG_FIELD_KEY, CONFIG_FIELD_TYPE, CONFIG_FIELD_NAME, NON_CONFIG_FIELD_NAME);
 
   private static void withCompiledSource(String inputSource, Consumer<String> testBody) throws IOException {
     Compilation result = Compiler.javac()
         .withProcessors(new ConfigProcessor())
         .compile(JavaFileObjects.forSourceString(INPUT_SOURCE_NAME, inputSource));
 
-    assertThat(result.status()).isEqualTo(Status.SUCCESS);
+    assertThat(result.status())
+        .as("Failed with: %s", result.errors())
+        .isEqualTo(Status.SUCCESS);
+
     assertThat(result.generatedSourceFiles()).hasSize(1);
 
     String generatedSource = IOUtils.toString(result.generatedSourceFiles().get(0).openInputStream(), UTF_8);
     testBody.accept(generatedSource);
   }
 
-  @Test
-  public void shouldGenerateClassWithExpectedNameAndModifiers() throws IOException {
+  @Nested
+  class WhenSuccessfullyCompilingConfigInterface {
 
-    withCompiledSource(DEFAULT_INPUT,
-        actualSource -> assertThat(actualSource).containsSequence("public final class " + ConfigSpec.CLASS_NAME)
-    );
+    @Test
+    public void generateClassWithExpectedNameAndModifiers() throws IOException {
+      withCompiledSource(DEFAULT_INPUT,
+          actualSource -> assertThat(actualSource).contains("public final class " + ConfigSpec.CLASS_NAME)
+      );
 
+    }
+
+    @Test
+    public void generateClassThatDeclaresAndInitializesFinalFieldsForEachAnnotatedMethods() throws IOException {
+
+      withCompiledSource(DEFAULT_INPUT,
+          actualSource -> assertThat(actualSource.replaceAll("\\n", " "))
+              .contains("private final " + CONFIG_FIELD_TYPE + " " + CONFIG_FIELD_NAME)
+              .containsPattern(CONFIG_FIELD_NAME + " = [^null].*")
+              .doesNotContain(NON_CONFIG_FIELD_NAME)
+      );
+
+    }
+
+    @Test
+    public void generateClassThatImplementsInterfaceAndOverridesAnnotatedMethods() throws IOException {
+
+      withCompiledSource(DEFAULT_INPUT,
+          actualSource -> assertThat(actualSource.replaceAll("\\n", " "))
+              .contains(ConfigSpec.CLASS_NAME + " implements " + INPUT_SOURCE_NAME)
+              .containsPattern("\\@Override(\\s+)public " + CONFIG_FIELD_TYPE + " " + CONFIG_FIELD_NAME + "\\(\\)")
+              .contains("return " + CONFIG_FIELD_NAME)
+      );
+
+    }
+
+    @Test
+    public void generateClassThatImplementsStaticMethodToInitializePropertyFields() throws IOException {
+
+      withCompiledSource(DEFAULT_INPUT,
+          actualSource -> assertThat(actualSource.replaceAll("\\n", " "))
+              .contains("import com.typesafe.config.Config;")
+              .containsPattern("private static " + CONFIG_FIELD_TYPE +
+                  " " + ANY_NAME + capitalize(CONFIG_FIELD_NAME) + "\\((final )?Config " + ANY_NAME + "\\)")
+              .containsPattern(
+                  "return " + ANY_NAME + "\\.get" + CONFIG_FIELD_TYPE + "\\(\"" + CONFIG_FIELD_KEY + "\"\\)"
+              )
+      );
+
+    }
   }
 }
