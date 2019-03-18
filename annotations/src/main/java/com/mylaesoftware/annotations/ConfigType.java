94e9cdc1a9f4773882eefe010f663788c9cbf635
Claudio Scandura <cl.scandura@gmail.com>
2019-03-18 23:46:13 +0000

    Add javadocs for annotations.

--- annotations/src/main/java/com/mylaesoftware/annotations/ConfigType.java
+++ annotations/src/main/java/com/mylaesoftware/annotations/ConfigType.java
@@ -2,6 +2,7 @@ package com.mylaesoftware.annotations;
 
 import com.mylaesoftware.validators.ConfigValidator;
 import com.mylaesoftware.validators.NoValidation;
+import com.typesafe.config.Config;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -9,13 +10,37 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
+/**
+ * Annotation used to mark interfaces as config types
+ *
+ * <p>It generates the necessary code to implement the annotated interface.
+ *
+ * @author Claudio Scandura
+ */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface ConfigType {
 
+  /**
+   * The optional context path of this config type.
+   *
+   * @return context path of this config type
+   */
   String contextPath() default "";
 
+  /**
+   * Optional validator to use verify the parsed config type.
+   *
+   * @return The validator to use verify the config type
+   */
   Class<? extends ConfigValidator<?>>[] validatedBy() default NoValidation.class;
 
+  /**
+   * Indicates whether {@link ConfigValue ConfigValues} parsing in this <tt>ConfigType</tt> should fall back to
+   * {@link com.typesafe.config.ConfigBeanFactory#create(Config, Class)} when a mapper cannot be found.
+   *
+   * @return true when mapping should default to {@link com.typesafe.config.ConfigBeanFactory#create(Config, Class)}
+   *         when a mapper cannot be found otherwise false.
+   */
   boolean fallbackToBeanMapper() default false;
 }
--- annotations/src/main/java/com/mylaesoftware/annotations/ConfigValue.java
+++ annotations/src/main/java/com/mylaesoftware/annotations/ConfigValue.java
@@ -11,13 +11,38 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
+/**
+ * Annotation used for methods of interfaces annotated with {@link ConfigType @ConfgType}.
+ *
+ * <p>It generates the necessary code to read the property at the specified path and assign it to a constant.</p>
+ *
+ * <p>Unless a custom mapper is specified, one of the basic mapper matching the return type of the method is used
+ * to parse the property. If no satisfying mapper is found the compilation will fail with an error. </p>
+ *
+ * @author Claudio Scandura
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.SOURCE)
 public @interface ConfigValue {
 
+  /**
+   * The path of the property where to find this config value.
+   *
+   * @return The path of the property where to find this config value
+   */
   String atPath();
 
+  /**
+   * Optional custom mapper to use to parse the config value.
+   *
+   * @return The custom mapper to use to parse the config value
+   */
   Class<? extends ConfigMapper<?>> mappedBy() default NoMapper.class;
 
+  /**
+   * Optional validator to use verify the parsed value.
+   *
+   * @return The validator to use verify the parsed value
+   */
   Class<? extends ConfigValidator<?>>[] validatedBy() default NoValidation.class;
 }
